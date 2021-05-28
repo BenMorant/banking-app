ff0b45a2b87332ae038403560593e80e71238208
Ben Mor </>
2021-05-28 15:40:15 +0200

    backend preparation (Spring initializr) frontend preparation (create-react-app)

---
+++ backend/src/main/java/com/benmorant/bankingapp/backend/BankingappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.benmorant.bankingapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingappBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/benmorant/bankingapp/backend/BankingappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benmorant.bankingapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
